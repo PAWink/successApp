faa882f66e851da34c2bb1d54aafc00721c9c8f5
PAWink <mysisterwinkwink@gmail.com>
2022-11-27 11:58:20 +0700

    showLoginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:success_app/helper/helper_function.dart';
+import 'package:success_app/pages/auth/login_page.dart';
 import 'package:success_app/pages/home_page.dart';
 import 'package:success_app/shared/constants.dart';
 
@@ -15,21 +17,42 @@ void main() async {
             appId: Constants.appId,
             messagingSenderId: Constants.messagingSenderId,
             projectId: Constants.projectId));
-    runApp(const MyApp());
   } else {
     //รันบนแอนดรอยและไอโอเอส
     await Firebase.initializeApp();
   }
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSigned = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        _isSigned = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: _isSigned ? const HomePage() : const LoginPage(),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(child: Text('HomePage')),
+    );
   }
 }
