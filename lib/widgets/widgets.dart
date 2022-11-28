b7105a9f00b4ec683c91d9ed55b5112bf627ed9e
PAWink <mysisterwinkwink@gmail.com>
2022-11-28 09:13:58 +0700

    logout

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -22,3 +22,19 @@ void nextScreenReplace(context, page) {
   Navigator.pushReplacement(
       context, MaterialPageRoute(builder: (context) => page));
 }
+
+void showSnackBar(context, color, message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      message,
+      style: const TextStyle(fontSize: 15),
+    ),
+    backgroundColor: color,
+    duration: const Duration(seconds: 2),
+    action: SnackBarAction(
+      label: 'OK',
+      onPressed: () {},
+      textColor: Colors.white,
+    ),
+  ));
+}
